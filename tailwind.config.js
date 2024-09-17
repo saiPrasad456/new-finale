21bed9a441c1f104b637d61a30c77d4b94eb6a2d
saiPrasad456 <spkar2004@gmail.com>
2024-09-17 22:45:22 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',
+    './public/index.html',
+    // add other paths if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
